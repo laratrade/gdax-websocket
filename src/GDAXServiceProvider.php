5467508a93fa70d8a7d3845f3c368bebbbf0e4b6
Nasyrov Evgenii <inasyrov@ya.ru>
2018-05-13 15:37:57 +0100

    WIP service provider

--- src/GDAXServiceProvider.php
+++ src/GDAXServiceProvider.php
@@ -13,6 +13,68 @@ class GDAXServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->configure()
+             ->offerPublishing()
+             ->registerServices()
+             ->registerCommands();
+    }
+
+    /**
+     * Setup the configuration.
+     *
+     * @return $this
+     */
+    protected function configure(): self
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/gdax.php',
+            'gdax'
+        );
+
+        return $this;
+    }
+
+    /**
+     * Setup the resource publishing groups.
+     *
+     * @return $this
+     */
+    protected function offerPublishing(): self
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/gdax.php' => config_path('gdax.php'),
+            ], 'gdax');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Register the services.
+     *
+     * @return $this
+     */
+    protected function registerServices(): self
+    {
         //
+
+        return $this;
+    }
+
+    /**
+     * Register the commands.
+     *
+     * @return $this
+     */
+    protected function registerCommands(): self
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                //
+            ]);
+        }
+
+        return $this;
     }
 }
