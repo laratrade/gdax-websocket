2bdaa20a9d664051959aa4046abb359b6150fc76
Nasyrov Evgenii <inasyrov@ya.ru>
2018-05-13 15:23:22 +0100

    Add the gdax service provider

---
+++ src/GDAXServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Laratrade\GDAX;
+
+use Illuminate\Support\ServiceProvider;
+
+class GDAXServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
